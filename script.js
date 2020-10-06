fdc8c4239476344aef2d363eecc63ee04e1a9d22
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-10-06 16:33:57 -0400

    added search history list, need to add local storage for page refresh

--- script.js
+++ script.js
@@ -12,27 +12,15 @@ $(document).ready(function() {
     var UVqueryURL;
     var lat;
     var lon;
+    var cityArray = [];
     
-
-    $("#searchBtn").on("click", function() {
-        city = $("#city").val();
-        var cityObject = JSON.parse(localStorage.getItem(city));
-        console.log(cityObject);
-        var lat = cityObject.coord.lat;
-        var lon = cityObject.coord.lon;
-        queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&appid=" + APIKey + "&units=imperial";
-        UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
-        // console.log(queryURL);
-        getCityData(APIKey, queryURL, city);
-        getUVIndex(APIKey, UVqueryURL, lat, lon);
-    })
-
     function getCityData(APIKey, queryURL, city) {
         $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(response) {
-            // cityObject = response;
+            var cityObject = response;
+            console.log(cityObject);
             localStorage.setItem(city, JSON.stringify(response));
             //   var temp = response.main.temp;
             // temp = (temp - 273.15) * 1.80 + 32;
@@ -45,19 +33,68 @@ $(document).ready(function() {
         // var newRow = $("<tr>");
         // newRow.append($("<td>" + location + "</td>"));
         // newRow.append($("<td>" + location + "</td>"));
-        $("#windSpeed").text("Wind Speed: " + response.wind.speed);
-        $("#humidity").text("Humidity: " + response.main.humidity);
-        $("#temperature").text(" Temperature: " + response.main.temp);
+        $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+        $("#humidity").text("Humidity: " + response.main.humidity + " %");
+        $("#temperature").text(" Temperature: " + response.main.temp + " ^F");
         $("#cityAndDate").text(response.name + " (" + dateDisplay + ")");
+        UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIKey;
+        getUVIndex(APIKey, UVqueryURL);
         });
     }
-    function getUVIndex(APIKey, UVqueryURL, lat, lon) {
+    $("#searchBtn").on("click", function() {
+        city = $(".city").val();
+        queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&appid=" + APIKey + "&units=imperial";
+        
+        getCityData(APIKey, queryURL, city);
+        // localStorage.setItem(city, JSON.stringify(cityObject));
+        if (cityArray.indexOf(city) != -1) {
+            cityArray = deleteElement(cityArray.indexOf(city), cityArray);
+        }
+        cityArray.push(city);
+        console.log(cityArray);
+
+        updateHistory();
+    });
+
+    function deleteElement(index, array) {
+        var tempArray = [];
+        for (var i = 0; i < array.length; i++) {
+            if (index == i) {
+                continue;
+            } else {
+                tempArray.push(array[i]);
+            }
+        }
+        return tempArray;
+    }
+    function updateHistory() {
+        $("#searchHistory").empty();
+        for (var i = 0; i < cityArray.length; i++) {
+            var newBtn = $("<button>");
+            newBtn.addClass("history city");
+            newBtn.attr("data-name", cityArray[i]);
+            newBtn.text(cityArray[i]);
+            // newLi.append(newBtn);
+            $("#searchHistory").prepend(newBtn);
+        }
+    }
+    
+    function getUVIndex(APIKey, UVqueryURL) {
         $.ajax({
             url: UVqueryURL,
             method: "GET"
             }).then(function(response) {
+                // console.log(response);
                 var UVIndex = response.value;
-                $("#UVIndex").text("UV Index: " + UVIndex);
+                if (UVIndex > 6) {
+                    $("#UVNumber").addClass("hiUV");
+                } else if (UVIndex > 3) {
+                    $("#UVNumber").addClass("hiUV");
+                } else {
+                    $("#UVNumber").addClass("lowUV");
+                }
+                $("#UVIndex").text("UV Index:"); //not printing the UVindex. need to keep the number element separate for color coding
+                $("#UVNumber").text(UVIndex);
             });
     }
 });
